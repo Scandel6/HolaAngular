1e3ea01648bb2ba110694597b5fed2e3edcb2f5d
Salvador Candel <salvacandel@gmail.com>
2022-04-30 22:23:40 +0200

    Inyección en TS, usando métodos para navegar entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+//Router es un componente que permite navegar entre componentes
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HolaAngular';
+  
+  //private router:Router
+
+  //Podemos saltarnos la declaración externa de una variable, es global (a esto se le llama inyección)
+  constructor(private router:Router){
+    this.router= router;
+  }
+
+  navegarAHome(){
+    this.router.navigate(['']);
+  }
+
+  navegarAAcercaDe(){
+    this.router.navigate(['acerca']);
+  }
 }
