1690dd46e0969c5ce50aae19054551a71417814a
Salvador Candel <salvacandel@gmail.com>
2022-05-01 14:46:19 +0200

    Using Query to get a variable called modo

--- src/app/articulo/articulo-list/articulo-list.component.ts
+++ src/app/articulo/articulo-list/articulo-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'; //Para navegar entre componentes
+import { Router, ActivatedRoute } from '@angular/router'; //Para navegar entre componentes
 
 @Component({
   selector: 'app-articulo-list',
@@ -8,15 +8,21 @@ import { Router } from '@angular/router'; //Para navegar entre componentes
 })
 export class ArticuloListComponent implements OnInit {
 
+  modo?: string;//? porque no sabemos si nos va a llegar
+
   constructor(
-    private rotuer:Router
-  ) { 
+    //Inyectamos router y route
+    private rotuer: Router,
+    private route: ActivatedRoute
+  ) {
   }
 
   ngOnInit(): void {
+    //Para cambiar de "string | null" a "string | undefined"
+    this.modo = (this.route.snapshot.queryParamMap.get("modo")) ?? undefined;
   }
 
-  public navegarAFicha(idArticulo: number):void {
+  public navegarAFicha(idArticulo: number): void {
     //Navegamos al componente de path articulo-ficha pasándole el idArticulo a articulo-form
     this.rotuer.navigate(['articulo-ficha', idArticulo]);
   }
