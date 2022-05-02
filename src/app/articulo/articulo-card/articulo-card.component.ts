2bb5057ea7844f10b3a86d58313de9852794e86d
Salvador Candel <salvacandel@gmail.com>
2022-05-02 20:35:00 +0200

    Quitada la toma de decisiones de card, ahora la list se encarga del funcionamiento de la clase hija a través de un evento

--- src/app/articulo/articulo-card/articulo-card.component.ts
+++ src/app/articulo/articulo-card/articulo-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Articulo } from '../articulo.model';
 
 @Component({
@@ -8,10 +8,15 @@ import { Articulo } from '../articulo.model';
 })
 export class ArticuloCardComponent implements OnInit {
   @Input() articulo?: Articulo; // Se le pasa a través de articulo-list.component.html, quien lo recive por su TypeScript
+  @Output() click: EventEmitter<number> = new EventEmitter<number>(); //Enviamos un click como evento para que sea el padre de esta clase quien gestione lo que esta va a mostrar
 
   constructor() {  }
 
   ngOnInit(): void {
   }
 
+  // Hacemos que cuando pulsemos el botón mandamos un mensaje con el identificador id de artículo
+  public pulsarBoton(): void{
+    this.click.emit(this.articulo?.getId());
+  }
 }
